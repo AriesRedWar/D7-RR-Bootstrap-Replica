8b8794dd6e9b0b5cef26f6118c69fdfebd26c536
AriesRedWar <philipjbowman5@gmail.com>
2022-09-29 17:55:05 -0400

    instaCart replication of view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import TopNavbar from "./Components/Navbar";
+import Background from "./Components/Background";
+import StoreListings from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopNavbar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
